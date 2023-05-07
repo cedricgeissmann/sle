11d0df829889966a96aec19b801748825919be19
Cedric Geissmann <cedric.geissmann@gmail.com>
2023-05-07 16:50:31 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Nav from './Nav.jsx'
+import Main from './Main.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Nav />
+      <Main />
+    </>
+  )
+}
+
+export default App
---
+++ src/Nav.jsx
@@ -0,0 +1,12 @@
+import './Nav.css'
+
+function Nav() {
+
+  return (
+    <nav>
+      <h1>Selbstlerneinheit</h1>
+    </nav>
+  )
+}
+
+export default Nav
