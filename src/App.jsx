52e95a4d49886ed8e092367fbe4008e88d7388a6
Cedric Geissmann <cedric.geissmann@gmail.com>
2023-05-12 14:39:40 +0200

    Change chapter and local storrage.

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,33 @@ import Nav from './Nav.jsx'
 import Main from './Main.jsx'
 import Side from './Side.jsx'
 
-import {useState} from 'react'
+import {getSessionStorageOrDefault} from './utils.js'
+
+import {useEffect, useState} from 'react'
 
 function App() {
 
-  const [chapter, setChapter] = useState(1)
+  const [chapter, setChapter] = useState(
+    getSessionStorageOrDefault('chapter', 1)
+  )
+  useEffect(() => {
+    sessionStorage.setItem('chapter', chapter)
+  }, [chapter])
+
+  const [finishedChapter, setFinishedChapter] = useState(
+    getSessionStorageOrDefault('finishedChapter', 0)
+  )
+  useEffect(() => {
+    sessionStorage.setItem('finishedChapter', finishedChapter)
+  }, [finishedChapter])
+
 
   return (
     <>
       <Nav />
       <div className='container'>
         <Side chapter={chapter} onChapterChange={setChapter} />
-        <Main chapter={chapter} />
+        <Main chapter={chapter} onChapterChange={setChapter} onChapterFinish={setFinishedChapter} />
       </div>
     </>
   )
--- src/Main.jsx
+++ src/Main.jsx
@@ -1,10 +1,28 @@
 import './Main.css'
 
-function Main({chapter}) {
+function Main({chapter, onChapterChange, onChapterFinish}) {
+
+  function nextChapter() {
+    if (chapter >= 4) return
+    onChapterChange(chapter + 1)
+    onChapterFinish(chapter + 1)
+  }
+
+  function prevChapter() {
+    if (chapter <= 1) return
+    onChapterChange(chapter - 1)
+  }
 
   return (
     <main>
+      <div className="chapter">
       <h2>Chapter {chapter}</h2>
+      </div>
+
+      <div className='footer'>
+        <button onClick={prevChapter}>Zurück</button>
+        <button onClick={nextChapter}>Weiter</button>
+      </div>
     </main>
   )
 }
