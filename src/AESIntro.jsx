efbc06b9a8c99e766ee5596ec0e316717d167b7f
Cedric Geissmann <cedric.geissmann@gmail.com>
2023-06-15 18:38:20 +0200

    remove unused pw box

--- src/AESIntro.jsx
+++ src/AESIntro.jsx
@@ -1,18 +1,11 @@
 import Hint from './Hint.jsx'
-import { useState } from 'react'
-import PWComponent from './PWComponent.jsx'
 
 function AESIntro() {
-
-  const [wordLength, setWordLength] = useState(1)
 
   return (
     <>
       <section>
         <h3>Das AES-Verfahren</h3>
-        <PWComponent>
-          <h1>WOW!!!!</h1>
-        </PWComponent>
         <p>
           <b>AES</b> steht für Advanced Encryption Standard und ist eines der meist genutzten symmetrischen Verschlüsselungsverfahren. AES gibt es in verschiedenen Varianten, wir schauen hier aber nur AES-128 an. Dieses reicht völlig aus von der Sicherheit. AES wird immer verwendet wenn eine <b>https</b>-Verbindung zu einer Webseite erstellt wird. Es ist somit die Grundlage für sicheren surfen im Internet und muss deshalb auch sehr schnell sein. 
         </p>
